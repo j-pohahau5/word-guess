b427becb612216a0a60efd97c8c65569995ce833
BrettSantor <b.santor27@gmail.com>
2023-01-03 15:20:13 -0800

    add dispalySpace function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,29 @@ var timeLeft = document.querySelector(".timeNum");
 var userScore = document.querySelector(".scoreNum");
 var timerInterval;
 var seconds = 60;
+var wordBank = [ 
+    ["t", "h", "e", "r", "e"],
+    ["b", "a", "c", "k"],
+    ["a", "g", "a", "i", "n"]
+];
+console.log(wordBank[0])
 
-startButton.addEventListener("click", setTimer)
 
 function setTimer(){
+    clearInterval(timerInterval)
+    seconds = 60
     timerInterval = setInterval(function(){
-        seconds--
-        timeLeft.textContent = seconds
+        seconds--;
+        timeLeft.textContent = seconds;
         if(seconds <= 0){
-            clearInterval(timerInterval)
+            clearInterval(timerInterval);
         }
         
        }, 1000) };
+
+       function displaySpaces() {
+       var randomVal = wordBank[Math.floor(wordBank.length * Math.random())];
+       wordSpace.textContent = randomVal;
+       }
+       startButton.addEventListener("click", setTimer)
+       startButton.addEventListener("click",displaySpaces)
