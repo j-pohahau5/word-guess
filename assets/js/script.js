0023da307acc8f6859c2363a29667d4c2602daf5
Jonathan Pohahau <jonathanpohahau@Jonathans-MBP.attlocal.net>
2023-01-03 16:12:42 -0800

    added content to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,39 @@
 var startButton = document.querySelector(".start");
 var wordSpace = document.querySelector(".spaces");
 var timeLeft = document.querySelector(".timeNum");
-var userScore = document.querySelector(".scoreNum");
+var userWins = document.querySelector(".winNum");
+var userLosses = document.querySelector(".lossNum");
+
+var chosenWord = "";
+var numBlanks = 0;
+var winCounter = 0;
+var loseCounter = 0;
+var isWin = false;
 var timerInterval;
 var seconds = 60;
-var wordBank = [ 
-    ["t", "h", "e", "r", "e"],
-    ["b", "a", "c", "k"],
-    ["a", "g", "a", "i", "n"]
-];
-console.log(wordBank[0])
+var wordBank = [ "there", "back", "again"];
 
+var lettersInWord = [];
+var blanksLetters = [];
+
+function init(){
+    getWins();
+    getlosses();
+}
+
+function winGame() {
+    wordSpace.textContent = "You Won"
+    winCounter++
+    startButton.disabled = false;
+    setWins()
+}
+
+function loseGame() {
+    wordSpace.textContent = "You Lose"
+    loseCounter++
+    startButton.disabled = false;
+    setLosses()
+}
 
 function setTimer(){
     clearInterval(timerInterval)
@@ -18,15 +41,67 @@ function setTimer(){
     timerInterval = setInterval(function(){
         seconds--;
         timeLeft.textContent = seconds;
+
         if(seconds <= 0){
             clearInterval(timerInterval);
+            loseGame()
         }
-        
+
+        if(isWin && seconds > 0) {
+            clearInterval(timerInterval)
+            winGame()
+        }        
        }, 1000) };
 
        function displaySpaces() {
        var randomVal = wordBank[Math.floor(wordBank.length * Math.random())];
-       wordSpace.textContent = randomVal;
+       lettersInWord = randomVal.split("")
+       numBlanks = lettersInWord.length
+       blanksLetters = []
+       for (var i = 0; i < numBlanks; i++){
+        blanksLetters.push("_")
        }
+       wordSpace.textContent = blanksLetters.join(" ");
+       }
+
+       function setWins() {
+        userWins.textContent = winCounter;
+        localStorage.setItem("winCount", winCounter);
+      }
+      
+      function setLosses() {
+        userLosses.textContent = loseCounter;
+        localStorage.setItem("loseCount", loseCounter);
+      }
+
+      function getWins() {
+        var storedWins = localStorage.getItem("winCount");
+        if (storedWins === null) {
+          winCounter = 0;
+        } else {
+          winCounter = storedWins;
+        }
+        userWins.textContent = winCounter;
+      }
+
+      function getlosses() {
+        var storedLosses = localStorage.getItem("loseCount");
+        if (storedLosses === null) {
+          loseCounter = 0;
+        } else {
+          loseCounter = storedLosses;
+        }
+        userLosses.textContent = loseCounter;
+      }
+
+      
+      
+      
+      
+      
+
        startButton.addEventListener("click", setTimer)
        startButton.addEventListener("click",displaySpaces)
+
+
+    
